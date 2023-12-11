5dc204b6351db6dbe82cfc63833c2bffca0a4d38
barbaragrosjean <92367251+barbaragrosjean@users.noreply.github.com>
2023-12-11 13:44:16 +0100

    new main.py

--- SVM/main.py
+++ SVM/main.py
@@ -11,30 +11,38 @@ def main() :
            'scheduler param step size' : step_size,
            'scheduler param gamma': gamma}
   
-
-
   # ===== Data =====
   #file_path = 'drive/MyDrive/Project2/resampled_epochs_subj_0.pkl' #for the drive
-  file_path = 'data/resampled_epochs_subj_0.pkl'
+  file_path = 'SVM/resampled_epochs_subj_0.pkl'
   path_to_save = 'SVM/trials'
 
   epochs, labels = get_data(file_path, convention_neg=True, two_channels=False)
-  train_loader, val_loader, test_loader = get_dataloaders(epochs[:100], labels[:100], batch_size=batch_size)
+  train_loader, val_loader, test_loader = get_dataloaders(epochs, labels, batch_size=batch_size)
+  
+  #find the ratio that caracterize the balancy between the class 
+  item, weight_loss = balance_weight(labels)
 
   input_size = train_loader.dataset[:][0].shape[2]*train_loader.dataset[:][0].shape[0]
 
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
   # ===== Model =====
   #build a model
-  model = SVM(input_size, num_classes = 60, pixel_nb = 25)
+  SMVmodel = SVM(device, input_size, num_classes = 60, pixel_nb = 25)
+  SMVmodel = SMVmodel.to(device)
+  #model = SVM(input_size, num_classes = 60, pixel_nb = 25)
 
   #Train and validate the model
-  Training_results = train(model,train_loader,val_loader) #columns=['Pixel n°', 'Training loss','Learning rate history', 'Training accuracy', 'Validating accuracy']
+  print("Training ...")
+  Training_results = train(SMVmodel,train_loader,val_loader, device, weight_loss) #columns=['Pixel n°', 'Training loss','Learning rate history', 'Training accuracy', 'Validating accuracy']
 
   #Test the model
-  Testing_results = test(model, test_loader, path_to_save) #columns=['Testing singles F1', 'Testing singles accuracy', 'Testing full F1', 'Testing full acc']
+  print("Testing ...")
+  Testing_results = test(SMVmodel, test_loader, path_to_save, device) #columns=['Testing singles F1', 'Testing singles accuracy', 'Testing full F1', 'Testing full acc']
 
   # ===== Saving and Plots =====
   #save
+  print("Plot and save ...")
   save_trial(Training_results, Testing_results, param, path_to_save)
 
   #plot and save 
