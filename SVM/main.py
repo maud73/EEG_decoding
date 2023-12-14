bd7b80fd9ad387eb95c6bcf2fe30c206fbb72228
barbaragrosjean <92367251+barbaragrosjean@users.noreply.github.com>
2023-12-14 10:27:57 +0100

    right nb of epochs main.py

--- SVM/main.py
+++ SVM/main.py
@@ -8,8 +8,8 @@ def main() :
 
   epochs, labels = get_data(file_path, convention_neg=True, two_channels=False)
 
-  train_loader_hyp, val_loader_hyp, _ = get_dataloaders(epochs[:100], labels[:100], batch_size=batch_size)
-  train_loader, val_loader, test_loader = get_dataloaders(epochs[:200], labels[:200], batch_size=batch_size)
+  train_loader_hyp, val_loader_hyp, _ = get_dataloaders(epochs[:500], labels[:500], batch_size=batch_size)
+  train_loader, val_loader, test_loader = get_dataloaders(epochs, labels, batch_size=batch_size)
   
   #find the ratio that caracterize the balancy between the class 
   item, weight_loss = balance_weight(labels)
