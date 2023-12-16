920c9febc874805f970cffa9d75bd2ef3e33444d
maud73 <92534343+maud73@users.noreply.github.com>
2023-12-16 19:13:21 +0100

    reproducibility

--- SVM/reproducibility.py
+++ SVM/reproducibility.py
@@ -15,3 +15,10 @@ def set_random_seeds(seed=42):
 
     # Set seed for Optuna
     optuna.seed(seed)
+
+def seed_worker(worker_id):
+    # This code is adapted from https://pytorch.org/docs/stable/notes/randomness.html
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+    
