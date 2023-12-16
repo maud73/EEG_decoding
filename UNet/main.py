69eaf683aa390d38008fc3cb4eed904c8ac894fb
maud73 <92534343+maud73@users.noreply.github.com>
2023-12-16 20:58:21 +0100

    remove val and test

--- UNet/main.py
+++ UNet/main.py
@@ -65,17 +65,11 @@ def main():
 
         data_tr_val = {
         "final_train_acc": final_train_acc,
-        "val_acc": val_acc,
-        "val_soft_acc": val_soft_acc,
         "lr_history": lr_history,
         "train_loss_history": train_loss_history,
         "train_acc_history": train_acc_history,
         "train_soft_acc_history": train_soft_acc_history,
         "train_f1_history": train_f1_history,
-        "val_loss_history": val_loss_history,
-        "val_acc_history": val_acc_history,
-        "val_soft_acc_history": val_soft_acc_history,
-        "val_f1_history": val_f1_history,
         }
         # Save the training and validating data
         with open(file_path, 'wb') as fp: 
@@ -95,46 +89,6 @@ def main():
         file_path = f'UNet/Plots/{filename}.png'
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-        plt.figure(figsize=(6.4, 4.8*5))
-        plt.subplot(5, 1, 2)
-        plt.plot(t_train, train_acc_history, label="Train")
-        plt.plot(t_val, val_acc_history, label="Val")
-        plt.legend()
-        plt.xlabel("Epoch")
-        plt.ylabel("Accuracy")
-
-        plt.subplot(5, 1, 4)
-        plt.plot(t_train, train_f1_history, label="Train")
-        plt.plot(t_val, val_f1_history, label="Val")
-        plt.legend()
-        plt.xlabel("Epoch")
-        plt.ylabel("F1 score")
-
-        plt.subplot(5, 1, 1)
-        plt.plot(t_train, train_loss_history, label="Train")
-        plt.plot(t_val, val_loss_history, label="Val")
-        plt.legend()
-        plt.xlabel("Epoch")
-        plt.ylabel("Loss")
-
-        plt.subplot(5, 1, 5)
-        plt.plot(t_train, lr_history)
-        plt.xlabel("Epoch")
-        plt.ylabel("Learning Rate")
-
-        plt.subplot(5, 1, 3)
-        plt.plot(t_train, train_soft_acc_history, label="Train")
-        plt.plot(t_val, val_soft_acc_history, label="Val")
-        plt.legend()
-        plt.xlabel("Epoch")
-        plt.ylabel("Soft accuracy")
-        plt.savefig(file_path)
-
-
-        # === Test the best model ===
-        file_path = f'UNet/Plots/Tests/{filename}.png'
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        test(best_model=model, test_loader=test_loader, file_path=file_path)
 
 if __name__ == '__main__':
         main()
